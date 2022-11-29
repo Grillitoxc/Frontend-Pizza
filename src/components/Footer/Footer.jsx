68a84e7a5c07291965d6ddb58cbd9fe79d9d7210
Christopher Torres Aceituno <christopher.torres@usach.cl>
2022-11-29 14:23:26 -0300

    [feat]: New interactions in footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,41 +2,44 @@ import React from "react";
 import { Container, Row, Col, ListGroup, ListGroupItem } from "reactstrap";
 
 import logo from "../../assets/images/logo8.png";
+import "../../styles/footer.css";
 
 const Footer = () => {
   return (
-    <Container>
-      <Row>
-        <Col lg="3" md="4" sm="6">
-        <div className="logo footer__logo text-start">
-            <img src={logo} alt="logo" />
-            <h5>Delicious Pizza</h5>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis, libero dignissimos. Consequuntur, soluta autem. Fugit fugiat velit facere iure reiciendis quae natus, porro culpa cumque. Autem tempora quidem tenetur enim.</p>
-          </div>
-        </Col>
+    <footer className="footer">
+      <Container>
+        <Row>
+          <Col lg="3" md="4" sm="6">
+            <div className="footer__logo text-start">
+              <img src={logo} alt="logo" />
+              <h5>Delicious Pizza</h5>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis, libero dignissimos. Consequuntur, soluta autem. Fugit fugiat velit facere iure reiciendis quae natus, porro culpa cumque. Autem tempora quidem tenetur enim.</p>
+            </div>
+          </Col>
 
-        <Col lg="3" md="4" sm="6">
-          <h4>Horario de servicio</h4>
-          <ListGroup>
-            <ListGroupItem className="border-0">
-              <span>Lunes a Viernes</span>
-              <p>8:00am - 00:00am</p>
-            </ListGroupItem>
+          <Col lg="3" md="4" sm="6">
+            <h5>Horario de servicio</h5>
+            <ListGroup className="schedule__list">
+              <ListGroupItem className="schedule__item border-0 ps-0">
+                <span>Lunes a Viernes</span>
+                <p>8:00am - 00:00am</p>
+              </ListGroupItem>
 
-            <ListGroupItem className="border-0">
-              <span>Sábados, Domingos y festivos</span>
-              <p>10:00am - 18:00pm</p>
-            </ListGroupItem>
-          </ListGroup>
-          
-        </Col>
+              <ListGroupItem className="schedule__item border-0 ps-0">
+                <span>Sábados, Domingos y festivos</span>
+                <p>10:00am - 18:00pm</p>
+              </ListGroupItem>
+            </ListGroup>
 
-        <Col lg="3" md="4" sm="6"></Col>
+          </Col>
 
-        <Col lg="3" md="4" sm="6"></Col>
-      </Row>
+          <Col lg="3" md="4" sm="6"></Col>
 
-    </Container>
+          <Col lg="3" md="4" sm="6"></Col>
+        </Row>
+
+      </Container>
+    </footer>
   );
 };
 
